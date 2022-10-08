b944ffab56e51435958ae271630d8656cd3aea6e
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-10-08 08:15:31 +0300

    commit tree bebfc1f2635b2b6e6682453c0b50200155c6a895

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # post "/login", to: "sessions#create"
+  # delete "/logout",  to: "sessions#destroy"
+  # get "/me", to: "users#show"
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  get "/me", to: "users#show"
+
+  
 end
